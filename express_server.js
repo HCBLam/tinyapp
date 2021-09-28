a7dcd1816ba66481e556d17c2f1b5ecb26800c59
Hasan Lam <hcb.lam@gmail.com>
2021-09-28 03:16:43 +0000

    Add body-parser middleware and URL submission form

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -13,6 +15,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -22,15 +28,37 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-})
+function generateRandomString() {
+  Math.random().toString(36).slice(7)
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+// function generateRandomString() {
+//   let randomString = '';
+//   for (let i = 0; i < 6; i++) {
+//     randomString += Math.round(Math.random() * 6).toString();
+//     console.log(`Random number [i]: ${Math.round(Math.random() * 6).toString()}`);
+//   }
+//   return randomString;
+// }
+// generateRandomString()
