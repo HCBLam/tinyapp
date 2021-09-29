022130a458d561e34b47174b7a948a1d3c9d7fb1
Hasan Lam <hcb.lam@gmail.com>
2021-09-29 16:44:26 +0000

    Add login and logout functions

--- express_server.js
+++ express_server.js
@@ -4,18 +4,47 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
+// ---------- Middleware Being Used ----------
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+// ---------- Helper Functions ----------
 function generateRandomString() {
   let randomString = Math.random().toString(36).slice(7);
   return randomString;
 }
+
+
+// ---------- Routes/Renders ----------
+
+// This is the route for the main '/urls' page with the list of short URLs and long URLs (urls_index).
+app.get('/urls', (req, res) => {
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  res.render('urls_index', templateVars);
+})
+
+// This is the route for the '/urls/new' page for creating a new short URL (urls_new).
+app.get('/urls/new', (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render('urls_new', templateVars);
+})
+
+// This is the route for the 'urls/:shortURL' individual page for each URL (urls_show).
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
+
+// ---------- Server CRUD Operations ----------
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,23 +58,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// This sends the main '/urls' page with the list of short URLs and long URLs (urls_index).
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-})
-
-// This sends the '/urls/new' page for creating a new short URL (urls_new).
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-})
-
-// This sends the 'urls/:shortURL' individual page for each URL (urls_show).
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
-});
-
 // This creates the new shortURL and adds it to the database; redirects to 'urls/:shortURL'
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
@@ -53,7 +65,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-// This redirects to the actual web page of the longURL.
+// This redirects the shortURL to the actual web page of each longURL.
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -79,7 +91,14 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username', req.body.username);
+  res.redirect('/urls');
+})
 
+
+
+// ---------- Server Up ----------
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
